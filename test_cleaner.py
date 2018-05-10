3b768505d8f0580e7c5ecae2c26d158e14f289ac
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-05-10 03:48:13 -0400

    fixes and agger

--- test_cleaner.py
+++ test_cleaner.py
@@ -14,8 +14,8 @@ def parserTaxi(id, data):
         tpep_pickup_datetime, tpep_dropoff_datetime, pickup_longitude, \
         pickup_latitude, dropoff_longitude, dropoff_latitude, Date, Weekday = i[1], i[2], i[5], i[6], i[9], i[10], \
         datetime.datetime.strptime(i[1], "%Y-%m-%d %H:%M:%S").date(), \
-        calendar.day_name[datetime.datetime.strptime("2016-06-09 21:06:36", "%Y-%m-%d %H:%M:%S").weekday()]
-        
+        calendar.day_name[datetime.datetime.strptime(i[1], "%Y-%m-%d %H:%M:%S").weekday()]
+       
         # rows required for the taxi data
         yield tpep_pickup_datetime, tpep_dropoff_datetime, pickup_longitude, \
         pickup_latitude, dropoff_longitude, dropoff_latitude, Date, Weekday
@@ -23,7 +23,8 @@ def parserTaxi(id, data):
 if __name__ == '__main__':
 	sc = pyspark.SparkContext()
 	sqlContext = SQLContext(sc)
-	taxi = sc.textFile('hdfs:///user/rbasnet000/Data/yellow_tripdata_2016-01.csv', use_unicode=False).cache() 
+	taxi = sc.textFile('hdfs:///user/rbasnet000/Data/yellow_tripdata_2016-12.csv', use_unicode=False).cache() 
 	taxiRDD = taxi.mapPartitionsWithIndex(parserTaxi)
 	df = sqlContext.createDataFrame(taxiRDD, ['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'Date', 'Weekday'])
-	df.coalesce(1).write.csv('hdfs:///user/rbasnet000/CleanData/yellow_tripdata_2016-01_clean', header=True)
+	df = df.select('Date').groupBy('Date').count()
+	df.coalesce(1).write.csv('hdfs:///user/rbasnet000/CleanData/yellow_tripdata_2016-12_AGG', header=True)
